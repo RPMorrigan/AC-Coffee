d063e24e1563376d8f96e55abee55a79c11d366a
Priscilla Morrigan <morriganpriscilla@gmail.com>
2025-08-15 10:23:04 -0600

    general update

---
+++ js/custom-contact.js
@@ -0,0 +1,23 @@
+let container = document.querySelector(".contact-page");
+let form = document.getElementById("ContactForm");
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+	e.preventDefault();
+
+	const data = new FormData(form);
+	const dataObj = Object.fromEntries(data.entries());
+
+    let name = dataObj.name;
+    let email = dataObj.email;
+    let comments = dataObj.comments;
+    let flavor = dataObj.flavor;
+    let buywork = dataObj.buywork;
+
+    let confirmationMsg = document.createElement("p");
+    confirmationMsg.textContent = `Thank you ${name}! We appreciate your interest in [buying/seeking emplayment]. Your favorite coffee flavor is ${flavor}, and your comments are ${comments}. We will soon contact you at ${email} or ${phone}.`;
+
+	console.log("our data:", dataObj);
+	form.reset();
+}
